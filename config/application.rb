d41f4c339bde8b857dc8d49264a88f9dbdebae6b
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-17 22:46:55 -0600

    fixing asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module LevGenius
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
-	  config.assets.precompile += %w(*.svg *.eot *.woff *.ttf)
+		config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
